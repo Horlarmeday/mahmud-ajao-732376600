3301231c32e08d5d52336a557f5a6a8460a87c96
Mahmud Ajao <ajaomahmud@gmail.com>
2024-02-13 18:39:40 +0100

    feat: Add axios to fetch data from API

--- src/common/external/axios.ts
+++ src/common/external/axios.ts
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import { BadRequestException, Logger } from '@nestjs/common';
 import { SUCCESS } from '../../core/constants';
+import { ResponseData } from './nftMarkets/event-response.type';
 
-export const get = async (url: string, headers: any, params = {}) => {
-  const logger = new Logger();
+export const get = async (url: string, params = {}) => {
+  const logger = new Logger('axios');
   try {
-    const response = await axios.get(url, { headers, params });
+    const response = await axios.get(url, { params });
     return {
       status: SUCCESS,
-      data: response.data,
+      data: response.data as ResponseData,
       statusCode: response.status,
     };
   } catch (e) {
     logger.error(`error`, e);
     logger.error(`An error occurred ${e?.message}`);
-    throw new BadRequestException(e.message);
+    throw new BadRequestException(e.message, { cause: e });
   }
 };
