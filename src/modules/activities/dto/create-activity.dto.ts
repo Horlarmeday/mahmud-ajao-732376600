73ac4bbf903e4924c1e0cfd39a6855f3ebc14d36
Mahmud Ajao <ajaomahmud@gmail.com>
2024-02-13 18:42:33 +0100

    feat: Add Activity DTO

--- src/modules/activities/dto/create-activity.dto.ts
+++ src/modules/activities/dto/create-activity.dto.ts
@@ -1,1 +1,9 @@
-export class CreateActivityDto {}
+export class CreateActivityDto {
+  contract_address: string;
+  token_index: string;
+  listing_price: number;
+  maker: string;
+  listing_from: Date;
+  listing_to: Date;
+  event_timestamp: string;
+}
---
+++ src/modules/activities/dto/create-token.dto.ts
@@ -0,0 +1,5 @@
+export class CreateTokenDto {
+  index: string;
+  contract_address: string;
+  current_price: number;
+}
